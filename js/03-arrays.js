fcba92b55de31a93c32b308d0f71c3046e9291cd
William8400 <wdonato02@gmail.com>
2025-07-30 17:26:57 -0300

    declarando arrays

--- js/03-arrays.js
+++ js/03-arrays.js
@@ -1,3 +1,16 @@
-'use strict'
+'use strict';
+
+// Declarando um array (vetor)
+let vocalistas = ["Coldplay", "Guns N Roses" , "Arctic Monkes"];
+
+// se quisermos visualizar a estrutura do array inteiro:
+// console.log(vocalistas);
+
+// Acessando um determinado elemento através do índice 
+console.log(vocalistas[0]);
+
+// O Coldplay está em trunê tocando paradise
+console.log(`O ${vocalistas[0]} está em turnê tocando paradise `);
+
 
 
