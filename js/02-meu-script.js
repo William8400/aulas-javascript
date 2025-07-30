2e1d4fc863fec51e2a77835a660be5c3f950ee40
William8400 <wdonato02@gmail.com>
2025-07-30 16:40:10 -0300

    exercícios strings

---
+++ js/02-meu-script.js
@@ -0,0 +1,28 @@
+/* Exercícios */
+// 1) Crie uma variável chamada "uc" e coloque nela o número da UC que estamos
+let uc = 2
+// 2) Crie uma variável chamada "competencia" e coloque nela uma frase que represente o que estamos estudando nesta UC.
+let competencia = 'Front-End com JavaScript.'
+// 3) Monte e mostre uma frase no console exibindo a UC e a competência. Exemplo: Estamos na UC2 estudando Front-End com JavaScript.
+console.log('Estamos na UC' + uc + ' estudando ' + competencia);  
+console.log(`Estamos na UC${uc} estudando ${competencia}`); 
+
+// ERRADO!
+//console.log('Gosto "muito" de paçoca!');
+
+// CORRETO! Aspas simples como comando, e duplas como texto 
+console.log('Gosto "muito" de paçoca!');
+
+// CORRETO! Aspas duplas como comando, e simples com texto 
+console.log("Gosto 'muito' de paçoca!");
+
+// CORRETO! Escapando as aspas como texto
+console.log("Gosto \"muito\" de paçoca!");
+
+
+
+
+
+
+
+
