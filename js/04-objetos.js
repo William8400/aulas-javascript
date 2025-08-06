b339294ec827c1f5a57de5a302645aa1f7f03ad5
William8400 <wdonato02@gmail.com>
2025-08-06 16:30:56 -0300

    Update 04-objetos.js

--- js/04-objetos.js
+++ js/04-objetos.js
@@ -3,7 +3,7 @@
 // Exemplo 1: objeto com dados de uma pessoa 
 
 const pessoa = {
-    nome: "William Hidalgo Donato" , 
+    nome: "William Hidalgo Donato", 
     idade: 21,
     cidade: "São Paulo",
     estado: "SP"
@@ -11,3 +11,6 @@ const pessoa = {
 
 console.log(pessoa);
 
+
+// acessamos as propriedades usando nomeDoObjeto.nomedaPropriedade
+console.log(`O ${pessoa.nome} tem ${pessoa.idade} anos`);
