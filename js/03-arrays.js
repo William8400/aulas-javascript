559494f6513a269d39fc5e6c02710ce8e17cf947
William8400 <wdonato02@gmail.com>
2025-08-06 14:23:56 -0300

    somente adicionei outra banda

--- js/03-arrays.js
+++ js/03-arrays.js
@@ -1,16 +1,16 @@
 'use strict';
 
 // Declarando um array (vetor)
-let vocalistas = ["Coldplay", "Guns N Roses" , "Arctic Monkes"];
+let vocalistas = ["Coldplay", "Guns N Roses" , "Arctic Monkes" , "Mamonas Assassinas"];
 
 // se quisermos visualizar a estrutura do array inteiro:
 // console.log(vocalistas);
 
 // Acessando um determinado elemento através do índice 
-console.log(vocalistas[0]);
+console.log(vocalistas[3]);
 
 // O Coldplay está em trunê tocando paradise
-console.log(`O ${vocalistas[0]} está em turnê tocando paradise `);
+console.log(`O ${vocalistas[3]} está em turnê tocando " Robocop Gay ".`);
 
 
 
