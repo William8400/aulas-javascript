dd8557d852dc4ffd101ed1870b0bab53025dba1d
William8400 <wdonato02@gmail.com>
2025-08-06 16:21:49 -0300

    Exemplo 1
    objeto com dados de uma pessoa

--- js/04-objetos.js
+++ js/04-objetos.js
@@ -1,3 +1,13 @@
 'use strict';
 
+// Exemplo 1: objeto com dados de uma pessoa 
+
+const pessoa = {
+    nome: "William Hidalgo Donato" , 
+    idade: 21,
+    cidade: "São Paulo",
+    estado: "SP"
+};
+
+console.log(pessoa);
 
