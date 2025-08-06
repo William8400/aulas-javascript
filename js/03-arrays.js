74e4b82569cb28d6f0b8ad7a67af942eeb256687
William8400 <wdonato02@gmail.com>
2025-08-06 14:32:12 -0300

    Relembrando os arrays (como criar)

--- js/03-arrays.js
+++ js/03-arrays.js
@@ -12,5 +12,16 @@ console.log(vocalistas[3]);
 // O Coldplay está em trunê tocando paradise
 console.log(`O ${vocalistas[3]} está em turnê tocando " Robocop Gay ".`);
 
+// Relembrando como criar/usar arrays (vetor)
+// No JS , vc pode colococar qualquer coisa em um array  
+
+let seila = 6;
+const coisas = [10, "Senac", "<h2> Oie </h2>", seila, 15.88];
+
+console.log(coisas[1]);
+console.log(coisas[4]);
+
+
+
 
 
