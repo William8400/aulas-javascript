9daeadac28aa4652c49ad776d2cbcdf99cef0744
William8400 <wdonato02@gmail.com>
2025-08-19 14:56:03 -0300

    Condicional composta

--- js/05-condicionais.js
+++ js/05-condicionais.js
@@ -11,10 +11,24 @@ let numero = 50;
 
 // Condicional SIMPLES (usa apenas if) 
 
-if(numero >= 10){
+if (numero >= 10) {
 
     console.log("Se vc está está vendo esta lendo essa mensagem é porque a condição é VERDADEIRA!");
-    
-
 }
 
+console.log("\nExemplo 2");
+
+let aluno = "James white Jason";
+let idade = 35;
+
+/* Lógica: verificar se o aluno é maior ou menor de idade.*/
+
+// Condicional COMPOSTA (if/else)
+
+if (idade < 18) {
+    console.log("Você é menor de idade!");
+
+} else {
+    console.log("Você é maior de idade!");
+
+}
