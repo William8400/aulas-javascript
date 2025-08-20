1d959dee719cfc169aaacce58f87d75a8eb1e571
William8400 <wdonato02@gmail.com>
2025-08-20 17:09:01 -0300

    --

--- js/07-loops.js
+++ js/07-loops.js
@@ -13,7 +13,7 @@ console.log('Exemplo 1: WHILE (enquanto)');
 let i = 1;
 while (i <= 5) {
 
-    console.log("Valor do i:" +i);
+    console.log("Valor do i:" + i);
     i++; // incremento 
     // i = i + 1; 
 }
@@ -21,22 +21,22 @@ while (i <= 5) {
 console.log("\nExemplo 2: FOR (PARA)");
 
 // for(inicialização da variável; condição; atualização)
-for( let j = 1; j <= 10; j++  ){
-    console.log(" J vale "+j);
+for (let j = 1; j <= 10; j++) {
+    console.log(" J vale " + j);
 }
 
 console.log("\nExemplo 3: loop regressivo");
 
-for( let k = 10; k >= 1; k--){
-    console.log(" K vale " +k);
+for (let k = 10; k >= 1; k--) {
+    console.log(" K vale " + k);
 }
 
 console.log("\nExemplo 4: loop e array");
 
 const frutas = ["Banana", "maçã", "Pêra", "kiwi", "Abacate"];
 // usando loop "for" tradicional
-for( let f = 0; f < frutas.length; f++ ){
-    console.log( frutas[f]);
+for (let f = 0; f < frutas.length; f++) {
+    console.log(frutas[f]);
 }
 
 console.log("\n------\n");
@@ -44,7 +44,7 @@ console.log("\n------\n");
 
 // Usando loop for/of "(exclusivo do JS)"
 // Usamos uma var/const (no caso , fruta) para acessar cada elemento do array (no caso, bandas) 
-for(const fruta of frutas ){
+for (const fruta of frutas) {
     console.log(fruta);
 }
 
@@ -58,7 +58,7 @@ const pessoa = {
 
 // Usando loop for/in para objetos (exclusivo par JS)
 
-for(const dados in pessoa){
+for (const dados in pessoa) {
     console.log(pessoa[dados]);
 }
 
@@ -66,8 +66,8 @@ for(const dados in pessoa){
 
 const clientes = [
     {
-       indentificador: 1,
-       nome: "Dio"
+        indentificador: 1,
+        nome: "Dio"
     },
 
     {
@@ -81,21 +81,21 @@ const clientes = [
     }
 ]
 
-for(const cliente of clientes){
-    console.log("- Cliente:" +cliente.nome+ ", id:" +cliente.indentificador);
+for (const cliente of clientes) {
+    console.log("- Cliente:" + cliente.nome + ", id:" + cliente.indentificador);
 }
 
-for(let b = 0; b < clientes.length; b++){
-    console.log("- Cliente:" +clientes[2].nome+ ",id:" +clientes[2].indentificador);
+for (let b = 0; b < clientes.length; b++) {
+    console.log("- Cliente:" + clientes[2].nome + ",id:" + clientes[2].indentificador);
 }
- 
+
 /* Cada objeto deverá ter uma propriedade "identificador" (com valores 1, 2 e 3) e uma propriedade "nome" (com os nomes dos clientes: "Dio", "Ozzy" e "Ian").
  
 2. Faça um loop (qualquer um dos que vimos) e mostre no console os dados de cada cliente conforme a seguir:
  
 - Cliente: Dio, id: 1
 - Cliente: Ozzy, id: 2
-- Cliente: Ian, id: 3 */ 
+- Cliente: Ian, id: 3 */
 
 
 
